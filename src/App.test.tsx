67925912f226919bf68d50f07ff7cbd4636febaf
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-11-21 21:29:49 +0200

    finish configure jest testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,5 +5,5 @@ import App from "./App";
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/november/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeTruthy();
 });
