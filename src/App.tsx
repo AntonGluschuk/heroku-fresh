a7d29a61e16225594f0123788ae58704b2231817
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-11-21 14:23:30 +0200

    change time in app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello it's 19 november 12:07</div>;
+  return <div className="App">Hello it's 21 november 14:23</div>;
 }
 
 export default App;
