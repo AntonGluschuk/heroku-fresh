5ebc71a38a085c315c1702a16b288bfb68dc4635
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-11-23 16:48:09 +0200

    change app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello it's 21 november 14:23</div>;
+  return <div className="App">Hello it's 23 november 16:47</div>;
 }
 
 export default App;
