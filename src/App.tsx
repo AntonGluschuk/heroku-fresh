49bf3a6bf811c563714825cab30814a3993f7238
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-11-24 10:48:40 +0200

    test workflow_run

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello it's 23 november 16:47</div>;
+  return <div className="App">Hello it's 23 november 20:45</div>;
 }
 
 export default App;
